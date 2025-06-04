1451048b90f98a0cd382dce56d95dc6f1a6f656f
Abhishek Sharma <abhishek.b.sharma1994@gmail.com>
2025-06-04 08:02:35 +0530

    refactoring the code removing extra or duplicate code

--- core/__init__.py
+++ core/__init__.py
@@ -7,7 +7,6 @@ Core modules for Doc2Train v2.0
 from .extractor import extract_content, extract_batch, get_supported_files
 from .generator import generate_training_data, generate_batch
 from .llm_client import call_llm, call_vision_llm, test_provider
-from .processor import process_files, save_results, get_processing_summary
 
 __all__ = [
     'extract_content',
